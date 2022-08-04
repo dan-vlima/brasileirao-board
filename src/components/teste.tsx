f2871cb1f6fc8dc00187e7129b3175c9a13eca35
Dan Lima <danlima.tech@gmail.com>
2022-08-04 03:08:52 -0300

    feat: create project basic structure

---
+++ src/components/teste.tsx
@@ -0,0 +1,11 @@
+const Teste = () => {
+    return (
+      <div className="flex flex-col space-y-64 bg-red-500">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    )
+  }
+  
+  export default Teste
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Teste from '../components/teste'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <Teste/>
+  )
+}
+
+export default Home
